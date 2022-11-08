2e8bf9d371ca9d90959d0cd2d615ebdd8bb14177
gtgambit <gtgambit@gmail.com>
2022-11-08 21:43:54 +0200

    pusn new updates

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+//Создай галерею с возможностью клика по её элементам и просмотра полноразмерного изображения в модальном окне. Посмотри демо видео работы галереи.
+//Выполняй это задание в файлах 01-gallery.html и 01-gallery.js. Разбей его на несколько подзадач:
+
+//Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+//Реализация делегирования на div.gallery и получение url большого изображения.
+//Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+//Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+//Замена значения атрибута src элемента <img> в модальном окне перед открытием. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+
+//Обрати внимание на то, что изображение обернуто в ссылку, а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу. Запрети это поведение по умолчанию.
+
+//Закрытие с клавиатуры
+//ВНИМАНИЕ
+//Этот функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
+
+//Добавь закрытие модального окна по нажатию клавиши Escape. Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно. У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const divEl = document.querySelector(".gallery");
+
+const gallery = galleryItems
+  .map(
+    (img) =>
+      `<div class="gallery__item"><a class="gallery__link" href="${img.original}"><img class="gallery__image" src="${img.preview}" data-source="${img.original}" alt="${img.description}"/></a></div>`
+  )
+  .join("");
+
+divEl.innerHTML = gallery;
+
+divEl.addEventListener("click", openImg);
+
+let instance;
+
+function openImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName === "DIV") {
+    return;
+  }
+  instance = basicLightbox.create(`
+    <div class="modal">
+        <img src=${event.target.dataset.source} width="80%" style="margin: auto; display: block"> 
+    </div>
+`);
+  instance.show();
+}
+
+//console.log(galleryItems);
